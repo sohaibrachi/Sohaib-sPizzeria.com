b723da2e6a854be566c5ad029e69d0f471ddcc1b
yazfield <hanifi.yazid@gmail.com>
2022-12-15 21:39:45 +0100

    refactor: create standalone components

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import './MenuItem.scss'
 
-function MenuItem({image, name, price}) {
+function MenuItem({ image, name, price }) {
   return (
     <div className='menuItem'>
-      <div style={{backgroundImage: `url(${image})` }}></div>
+      <div style={{ backgroundImage: `url(${image})` }}></div>
       <h1> {name}</h1>
       <p>${price} </p>
     </div>
-    
+
   )
 }
 
